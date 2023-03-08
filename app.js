8e774a6308a24870962b1196936f2f16591e8d47
brian.kim <brianhkim@gmail.com>
2023-03-08 15:58:49 -0600

    rough draft #6(got help)

--- app.js
+++ app.js
@@ -12,26 +12,30 @@ const gameState = {
 
 const board = document.querySelector(".board");
 
-board.addEventListener("click", (e) => {
+board.addEventListener("click", (event) => {
+  let coordinates = event.target.id.split("");
+  console.log(coordinates);
+  const i = Number(coordinates[0]);
+  const j = Number(coordinates[1]);
 
-  // WHat are we trying to do when we click on the board???
-  //change null to x or o
-
-  console.log(e.target.id);
-  // figure out how to use the id to set the gameState.board's correct positon to be the current player
-  gameState.board[i][j] = gameState.players[0];
-
-  // check for win
-
-  // switch the current player
-  function switchPlayer() {
-    if (gameState.board[i][j]=gameState.players[0]) {
-      gameState.board[i][j]=gameState.players[1]
-    } else if (gameState.board[i][j]=gameState.players[1]){
-      gameState.board[i][j]=gameState.players[0]
-    }
-  }
-  // change the currentPlayer Idx
+  gameState.board[i][j] = "x";
+  //   // WHat are we trying to do when we click on the board???
+  //   //change null to x or
+  board.innerHTML = null;
+  renderGame();
+  //   // figure out how to use the id to set the gameState.board's correct positon to be the current player
+  // gameState.board[i][j] = gameState.players[0];
+  //   // check for win
+  //   // switch the current player
+  //   function switchPlayer() {
+  //     //event listen on the board, specific part of the board, player 1 turn then make square x
+  //     if ((gameState.board[i][j] = gameState.players[0])) {
+  //       gameState.board[i][j] = gameState.players[1];
+  //     } else if ((gameState.board[i][j] = gameState.players[1])) {
+  //       gameState.board[i][j] = gameState.players[0];
+  //     }
+  //   }
+  //   // change the currentPlayer Idx
 });
 // const cell = gameState.board[i][j];
 const form1 = document.querySelector("#player1form");
@@ -59,7 +63,7 @@ let gameStatus = document.querySelector("#gameStatus");
 
 function renderStatus() {
   const playerStatus = document.createElement("ul");
-  playerStatus.append(playerNames);
+  playerStatus.append();
 }
 //i know i need to use innertext to refer to the input of currentplayer forms
 
@@ -69,17 +73,23 @@ function renderStatus() {
 //if player 1 wins +1 to his scoreboard, else if player 2 wins +1 to his scoreboard
 
 function renderGame() {
+  console.log("hit");
   // Call this function after you've changed your state values
-  for(let i = 0, i < gameState.board[i].length; i++){
-    let rowEl = gameState.board[i]
-    for(let j =0, i < gameState.board[j].length;j++){
-      let columnEl = gameState.board[j]
+  for (let i = 0; i < gameState.board.length; i++) {
+    let rowEl = gameState.board[i];
+    for (let j = 0; j < gameState.board[i].length; j++) {
+      let columnEl = gameState.board[j];
+      let square = document.createElement("div");
+      square.innerText = gameState.board[i][j];
+      square.id = `${i}${j}`;
+      square.className = "cell";
+      board.appendChild(square);
     }
   }
   // Make references to DOM elements, and set the innerText,
   // or innerHTML to reflect our gameState.board
 }
-
+renderGame();
 function checkRow(grid, i, j) {}
 function checkColumn(grid, i, j) {}
 function checkDiagonals(grid, i, j) {}
