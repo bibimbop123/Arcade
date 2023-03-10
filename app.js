ddc8c6f0c57a12d2ca3547043406eeb3fb0510d4
brian.kim <brianhkim@gmail.com>
2023-03-10 16:58:12 -0600

    switch player function

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 const board = document.querySelector(".board");
 
+const gameState = {
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  currentPlayer: "x",
+};
 for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
-    const cell = document.createElement("div)");
+    const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.id = `${i}-${j}`;
     board.append(cell);
+  }
+}
+board.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  const row = e.target.id[0];
+  const col = e.target.id[2];
+  gameState.board[row][col] = gameState.currentPlayer;
+  console.log("Game State: ", gameState);
+  renderboard();
+  switchPlayer();
+});
+function renderboard() {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      const cell = document.getElementById(`${i}-${j}`);
+      cell.innerText = gameState.board[i][j];
+    }
+  }
+}
+function switchPlayer() {
+  if (gameState.currentPlayer === "x") {
+    gameState.currentPlayer = "o";
+  } else {
+    gameState.currentPlayer = "x";
   }
 }
 // const board = document.querySelector(".board");
