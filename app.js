714ee360d72a10d81f0332c8023accdae8b6f5f7
brian.kim <brianhkim@gmail.com>
2023-03-10 14:36:34 -0600

    need id of cells in board

--- app.js
+++ app.js
@@ -1,50 +1,62 @@
-let gameState = {
-  board: [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ],
-  // Maybe a current Player?
-  // A game status?? 'isPlaying' or 'over'
-  // Any other data your game logic depends on?
-};
+const board = document.querySelector(".board");
+
 for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
-    const cell = document.createElement("div");
+    const cell = document.createElement("div)");
     cell.classList.add("cell");
     cell.id = `${i}-${j}`;
     board.append(cell);
   }
 }
+// const board = document.querySelector(".board");
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     const cell = document.createElement("div");
+//     cell.classList.add("cell");
+//     cell.id = `${i}-${j}`;
+//     board.append(cell);
+//   }
+// }
 
-const board = document.querySelector(".board");
-board.addEventListener("click", function (event) {
-  console.log(event.target);
-  // Figure out how to get the coordinates off event object (e.target.value)
-  // Use those coordinates to reference indexes in our gameState.board
-  // Set the position in our board to the current player
-});
+// let gameState = {
+//   board: [
+//     [null, null, null],
+//     [null, null, null],
+//     [null, null, null],
+//   ],
+//   // Maybe a current Player?
+//   // A game status?? 'isPlaying' or 'over'
+//   // Any other data your game logic depends on?
+// };
 
-function renderGame() {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      const cell = document.createElement("div");
-      cell.id = "${i}-${j}";
-      board.append(cell);
-    }
-    board.append.cell;
-}
-// Call this function after you've changed your state values
-// Make references to DOM elements, and set the innerText,
-// or innerHTML to reflect our gameState.board
+// const board = document.querySelector(".board");
+// board.addEventListener("click", function (event) {
+//   console.log(event.target);
+//   // Figure out how to get the coordinates off event object (e.target.value)
+//   // Use those coordinates to reference indexes in our gameState.board
+//   // Set the position in our board to the current player
+// });
 
-function switchPlayer() {
-  // ???
-}
+// function renderGame() {
+//   for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//       const cell = document.createElement("div");
+//       cell.id = "${i}-${j}";
+//       board.append(cell);
+//     }
+//     board.append.cell;
+// }
+// // Call this function after you've changed your state values
+// // Make references to DOM elements, and set the innerText,
+// // or innerHTML to reflect our gameState.board
 
-function checkWin() {
-  // Maybe this calls other helper functions?
-  // checkRow()
-  // checkColumn()
-  // checkDiagonals()
-}
+// function switchPlayer() {
+//   // ???
+// }
+
+// function checkWin() {
+//   // Maybe this calls other helper functions?
+//   // checkRow()
+//   // checkColumn()
+//   // checkDiagonals()
+// }
